cea9fa6e046aaad3882f4183b776910e8a1a7272
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-05-14 15:22:24 +0600

    available appointment slots api added

--- index.js
+++ index.js
@@ -38,11 +38,39 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services)
         })
+
+        // GET : filtered available services
+        app.get('/available', async(req, res)=>{
+          const date = req.query.date || "May 14, 2022" ;
+
+          // step 1: get all services
+          const services = await serviceCollection.find().toArray();
+
+          // step 2: get the booking of that day
+          const query = {date: date};
+          const bookings = await bookingCollection.find(query).toArray();
+
+          // step 3: for each service, find booking for that service
+          services.forEach(service =>{
+            const serviceBookings = bookings.filter(b => b.treatment === service.name);
+            const booked = serviceBookings.map(s => s.slot);
+            const available = service.slots.filter(s => !booked.includes(s));
+            service.available = available;
+          })
+
+          res.send(services);
+        })
+
         // POST : add new booking
         app.post('/booking', async(req,res) =>{
           const booking = req.body;
+          const query = { treatment: booking.treatment, date: booking.date, patientName : booking.patientName };
+          const exists = await bookingCollection.findOne(query);
+          if(exists){
+            return res.send({success: false, booking: exists })
+          }
           const result = await bookingCollection.insertOne(booking);
-          res.send(result);
+          return res.send({success: true, result});
         })
 
 
