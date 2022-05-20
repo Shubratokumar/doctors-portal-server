b8dc1d2062b0f0208ca97ec77e04e67851cccadc
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-05-20 11:03:26 +0600

    stripe added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const app = express();
 const nodemailer = require('nodemailer');
@@ -12,7 +13,6 @@ app.use(cors());
 app.use(express.json());
 
 // Database Connection : MongoDB
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xczhe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -152,6 +152,14 @@ async function run() {
           } else {
             return res.status(403).send({message: "Forbidden Access"});
           }
+        });
+
+        // GET : to find specific bookings
+        app.get("/booking/:id", verifyJWT, async(req,res)=>{
+          const id = req.params.id;
+          const query = {_id: ObjectId(id)};
+          const booking = await bookingCollection.findOne(query);
+          res.send(booking);
         })
 
         // POST : add new booking
